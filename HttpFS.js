0ac3896b29d23daa8047787cadee7039b0860149
Jesus Caño <jcanovel@gmail.com>
2017-01-16 14:38:17 +0100

    Add MKCOL

--- HttpFS.js
+++ HttpFS.js
@@ -76,3 +76,16 @@ methods.PUT = function (path, respond, request) {
     });
     request.pipe(outStream);
 };
+
+methods.MKCOL = function (path, respond) {
+    fs.stat(path, function (error, stats) {
+        if (error && error.code == "ENOENT")
+            fs.mkdir(path, respondErrorOrNothing(respond));
+        else if (error)
+            respond(500, error.toString());
+        else if (stats.isDirectory())
+            respond(204);
+        else
+            respond(400);
+    });
+};
